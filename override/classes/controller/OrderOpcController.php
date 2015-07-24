a70f47aa678075b63d77fe3e165f64f161bb7f27
JimNuttUp <jim@upcyclepost.com>
2015-07-24 12:24:19 -0700

    Override the override in OPC Controller

--- override/classes/controller/OrderOpcController.php
+++ override/classes/controller/OrderOpcController.php
@@ -23,7 +23,7 @@
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
-
+/*
 class OrderOpcController extends OrderOpcControllerCore
 {
 
@@ -44,3 +44,4 @@ class OrderOpcController extends OrderOpcControllerCore
     }
 
 }
+*/
