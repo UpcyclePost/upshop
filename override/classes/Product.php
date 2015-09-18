d69155a94360c15fc8e26652060dccdc3473ba60
JimNuttUp <jim@upcyclepost.com>
2015-09-18 14:44:10 -0700

    upmod v0.3.1.0 Previous and Next on product page scroll through shop not category

--- override/classes/Product.php
+++ override/classes/Product.php
@@ -5,29 +5,57 @@ class Product extends ProductCore
 
 	public function getAdjacentProducts()
 	{
-
 		//get the current position in the product's default category
-		$position  = Db::getInstance()->getValue('SELECT position FROM '._DB_PREFIX_.'category_product WHERE id_product = ' . (int)$this->id . ' AND id_category = ' . (int)$this->id_category_default);
+		//$position  = Db::getInstance()->getValue('SELECT position FROM '._DB_PREFIX_.'category_product WHERE id_product = ' . (int)$this->id . ' AND id_category = ' . (int)$this->id_category_default);
 
-		// var_dump($position);
-		// get products that are before and after
+		// get the seller for the current product
+		$sql = "SELECT s.id_seller
+				FROM "._DB_PREFIX_."marketplace_seller_product s 
+				JOIN "._DB_PREFIX_."marketplace_shop_product p ON s.id = p.marketplace_seller_id_product
+				WHERE p.id_product = " . (int)$this->id . ";";
 		
-		$previous = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow('
-			SELECT cp.id_product, pl.link_rewrite, cp.position, pl.name
-			FROM '._DB_PREFIX_.'category_product cp
-			LEFT JOIN '._DB_PREFIX_.'product_lang pl ON (cp.id_product = pl.id_product)
-			LEFT JOIN '._DB_PREFIX_.'product p ON (cp.id_product = p.id_product)
-			WHERE p.id_category_default = '.(int)$this->id_category_default.' AND (cp.position < '. (int)($position ) .' ) AND cp.id_category = ' . (int)$this->id_category_default .' AND pl.id_lang = '.(Context::getContext()->language->id).'
-			ORDER BY cp.position DESC');
+		echo "<!-- sql :";
+		echo ($sql);
+		echo "-->";
 
-		$next = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow('
-			SELECT cp.id_product, pl.link_rewrite, cp.position, pl.name
-			FROM '._DB_PREFIX_.'category_product cp
-			LEFT JOIN '._DB_PREFIX_.'product_lang pl ON (cp.id_product = pl.id_product)
-			LEFT JOIN '._DB_PREFIX_.'product p ON (cp.id_product = p.id_product)
-			WHERE p.id_category_default = '.(int)$this->id_category_default.' AND (cp.position > '. (int)($position ) .' ) AND cp.id_category = ' . (int)$this->id_category_default .' AND pl.id_lang = '.(Context::getContext()->language->id).'
-			ORDER BY cp.position ASC');
+		$seller_id  = Db::getInstance()->getValue($sql);
 
+		echo "<!-- seller_id :";
+		echo ($seller_id);
+		echo "-->";
+
+		// get products that are before and after
+		$previoussql = "SELECT pl.id_product, pl.link_rewrite, pl.name
+						FROM up_product_lang pl 
+						LEFT JOIN "._DB_PREFIX_."marketplace_shop_product p ON p.id_product = pl.id_product
+						LEFT JOIN "._DB_PREFIX_."marketplace_seller_product s ON p.marketplace_seller_id_product = s.id
+						WHERE s.id_seller = " . $seller_id . "
+						AND pl.id_product < " . (int)$this->id . "
+						ORDER BY pl.id_product DESC";
+		echo "<!-- previoussql : ";
+		echo ($previoussql);
+		echo "-->";
+		
+		$previous = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow($previoussql);
+		echo "<!-- previous";
+		print_r ($previous);
+		echo "-->";
+		
+		$nextsql = "SELECT pl.id_product, pl.link_rewrite, pl.name
+						FROM up_product_lang pl 
+						LEFT JOIN "._DB_PREFIX_."marketplace_shop_product p ON p.id_product = pl.id_product
+						LEFT JOIN "._DB_PREFIX_."marketplace_seller_product s ON p.marketplace_seller_id_product = s.id
+						WHERE s.id_seller = " . $seller_id . "
+						AND pl.id_product > " . (int)$this->id . "
+						ORDER BY pl.id_product ASC";
+		echo "<!-- nextsql";
+		echo ($nextsql);
+		echo "-->";
+
+		$next = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow($nextsql);
+		echo "<!-- next";
+		print_r($next);
+		echo "-->";
 
 		return array('previous' => $previous, 'next' => $next);
 	}
